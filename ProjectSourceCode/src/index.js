9e873328890ecda76844f598b5cb8a8fcb4739ef
William Brito <williamcebrito@gmail.com>
2026-04-02 01:33:15 -0600

    Moved middleware such that user data is accessible to all pages without conflicts

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -116,8 +116,15 @@ app.use(
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
-  })
+      })
 );
+
+//Makes user available to all templates
+app.use((req, res, next) => {
+  console.log("SESSION ON REQUEST:", req.session.user);
+  res.locals.user = req.session.user || null;
+  next();
+});
 
 app.use(
   bodyParser.urlencoded({
@@ -328,10 +335,10 @@ const auth = (req, res, next) => {
   next();
 };
 
-//Makes user available to all templates
-app.use((req, res, next) => {
-  res.locals.user = req.session.user || null;
-  next();
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/home');
+  });
 });
 
 //can only access friends page if authenticated
